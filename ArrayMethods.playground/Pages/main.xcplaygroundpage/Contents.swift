a54c6b617bb39d09917da3b38f227da2dd97eb7b
Asia <asia.elnemer@exa.com.sa>
2016-11-27 21:52:09 +0200

    solve

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -18,8 +18,10 @@
 
 
 
-
-
+var daysOfTheWeek = ["friday","saturday","sunday","monday","tuesday","wednesday","thursday","friday"]
+for the_day in daysOfTheWeek {
+    print("\(the_day)")
+}
 
 
 /*: question2
@@ -27,9 +29,10 @@
  */
 // write your code here
 
-
-
-
+let numDaysOfTheWeek = ["friday","saturday","sunday","monday","tuesday"];
+for (index , the_day) in numDaysOfTheWeek.enumerated(){
+    print("the day is \(the_day)  is in \(index)")
+}
 
 
 
@@ -38,7 +41,13 @@
  */
 // write your code here
 
-
+var emptyArray :[String] = []
+if !emptyArray.isEmpty {
+    print("It's not Empty")
+}
+else {
+    print("It's Empty")
+}
 
 
 
@@ -56,15 +65,13 @@
 
 
 
-
-
 /*: question5
  ### 5. Find out the size of the 'daysOfTheWeek' array created earlier, and print it to the screen.
  */
 // write your code here
 
 
-
+daysOfTheWeek.count
 
 
 
@@ -111,7 +118,10 @@
 // write your code here
 
 
-
+if daysOfTheWeek.count > 5 {
+    daysOfTheWeek.removeLast()
+    daysOfTheWeek.removeLast()
+}
 
 
 
