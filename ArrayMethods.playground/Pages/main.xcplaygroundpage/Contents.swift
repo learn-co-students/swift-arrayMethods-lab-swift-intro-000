60dc7b036306ab096b9885f568153613f90f56bd
schrist <SimonChrist@gmx.de>
2017-04-19 12:27:31 +0200

    My Solution

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,10 @@
 /*: question1
  ### 1. Create a changeable list for the five days of the week called 'daysOfTheWeek' and print each one by using a loop.
  */
-
+var daysOfTheWeek = ["Monday","Tuesday","Wendesday","Thursday","Friday",]
+for day in daysOfTheWeek {
+    print(day)
+}
 
 
 
@@ -27,7 +30,10 @@
  */
 
 
-
+let weekDays = ["Monday","Tuesday","Wendesday","Thursday","Friday"]
+for (index,day) in daysOfTheWeek.enumerated() {
+    print(index,day)
+}
 
 
 
@@ -36,10 +42,11 @@
 /*: question3
  ### 3. Create an empty array of strings called 'emptyArray' and check to see if it's empty, printing appropriate messages.
  */
+var emptyArray:[String] = []
 
-
-
-
+if emptyArray.isEmpty {
+    print("This array is empty.")
+}
 
 
 
@@ -49,8 +56,11 @@
  ### 4.  Create an array of strings called 'funColors'. The colors in the array should be "red", "blue" and "green". Check to see if it's NOT empty, printing an appropriate message.
  */
 
+var funColors = ["red","blue","green"]
 
-
+if !funColors.isEmpty {
+    print("This array is not empty.")
+}
 
 
 
@@ -62,7 +72,7 @@
  ### 5. Find out the size of the 'daysOfTheWeek' array created earlier, and print it to the screen.
  */
 
-
+print(daysOfTheWeek.count)
 
 
 
@@ -72,8 +82,7 @@
 /*: question6
  ### 6. Add the two days of the weekend to the 'daysOfTheWeek array and then loop through the array to print all the values.
  */
-
-
+daysOfTheWeek.append(contentsOf:["Saturday","Sunday"])
 
 
 
@@ -85,8 +94,8 @@
 /*: question7
  ### 7. Some people start the week on a Sunday. Remove Sunday from the end of the 'daysOfTheWeek' array and then re-add it to the beginning of the array.
  */
-
-
+var removed = daysOfTheWeek.removeLast()
+daysOfTheWeek.insert(removed, at: 0)
 
 
 
@@ -96,7 +105,9 @@
 /*: question8
  ### 8. Loop through the 'daysofTheWeek' array and print each value to the console as lower case.
  */
-
+for day in daysOfTheWeek {
+    print(day.lowercased())
+}
 
 
 
