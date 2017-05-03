15f4a22265628c0c1790b25fb6b755ee28934b7e
jcornelussen <j.cornelussen@planet.nl>
2017-05-03 23:09:15 +0200

    Done

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,33 +14,40 @@
 /*: question1
  ### 1. Create a changeable list for the five days of the week called 'daysOfTheWeek' and print each one by using a loop.
  */
+var daysOfTheWeek = Array<String>()
 
+daysOfTheWeek.append("Monday")
+daysOfTheWeek.append("Tuesday")
+daysOfTheWeek.append("Wednessday")
+daysOfTheWeek.append("Thursday")
+daysOfTheWeek.append("Friday")
 
-
-
-
+for (index, day) in daysOfTheWeek.enumerated() {
+  print("Workday \(index+1) is \(day)")
+}
 
 
 
 /*: question2
  ### 2. Create an unchanging list for the five days of the week called 'weekDays' and print each one by using a loop, prefixed by the position of the day of the week.
  */
+let weekDays = ["Monday", "Tuesday", "Wednessday", "Thursday", "Friday"]
 
-
-
-
-
-
+for (index, day) in weekDays.enumerated() {
+  print("\(index+1): \(day)") // Or index+2 if the day in the week was meant, assuming Sunday is day 1
+}
 
 
 /*: question3
  ### 3. Create an empty array of strings called 'emptyArray' and check to see if it's empty, printing appropriate messages.
  */
+var emptyArray: [String] = [] // Using the array before initializing results in a x-code error.
 
-
-
-
-
+if !emptyArray.isEmpty {
+  print("Not empty")
+} else {
+  print("Empty")
+}
 
 
 
@@ -48,24 +55,20 @@
 /*: question4
  ### 4.  Create an array of strings called 'funColors'. The colors in the array should be "red", "blue" and "green". Check to see if it's NOT empty, printing an appropriate message.
  */
+var funColors = ["red", "blue", "green"]
 
-
-
-
-
-
-
+if !funColors.isEmpty {
+  print("FunColors is not empty")
+} else {
+  print("FunColors is empty")
+}
 
 
 
 /*: question5
  ### 5. Find out the size of the 'daysOfTheWeek' array created earlier, and print it to the screen.
  */
-
-
-
-
-
+print("Number of workdays in week is: \(daysOfTheWeek.count)")
 
 
 
@@ -73,37 +76,29 @@
  ### 6. Add the two days of the weekend to the 'daysOfTheWeek array and then loop through the array to print all the values.
  */
 
+daysOfTheWeek.insert("Saturday", at: daysOfTheWeek.count)
+daysOfTheWeek.insert("Sunday", at: daysOfTheWeek.count)
 
-
-
-
-
-
+for (index, day) in daysOfTheWeek.enumerated() {
+  print("Day \(index+1) is \(day)")
+}
 
 
 
 /*: question7
  ### 7. Some people start the week on a Sunday. Remove Sunday from the end of the 'daysOfTheWeek' array and then re-add it to the beginning of the array.
  */
+var day = daysOfTheWeek.remove(at: 6)
+daysOfTheWeek.insert(day, at: 0)
 
-
-
-
-
-
-
-
+let indexOfSaturday = daysOfTheWeek.index(of: "Saturday")!
+print("Index of Saturday = \(indexOfSaturday)")
 /*: question8
  ### 8. Loop through the 'daysofTheWeek' array and print each value to the console as lower case.
  */
-
-
-
-
-
-
-
-
+for (index, day) in daysOfTheWeek.enumerated() {
+  print("\(day.lowercased())")
+}
 
 
 
